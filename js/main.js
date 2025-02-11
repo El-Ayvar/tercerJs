249c694153a0751ac8b8793387a425f277daf0a2
El-Ayvar <=>
2025-02-11 11:44:05 -0600

    tercer ejercicio de js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let primerNumero = 2;
+let segundoNumero = 3;
+
+let resultadoSuma = primerNumero + segundoNumero ;
+let resultadoResta = primerNumero - segundoNumero ;
+let resultadoMultiplicacion = primerNumero * segundoNumero ;
+let resultadoDivision = primerNumero / segundoNumero;
+
+console.log (resultadoDivision)
+
+console.log (resultadoMultiplicacion)
+
+console.log (resultadoResta)
+
+console.log (resultadoSuma)
